35bf665014bb279977289b4e64b53b2a3a2944df
nehakumarishaw <87537325+nehakumarishaw@users.noreply.github.com>
2024-12-13 21:27:42 +0530

    files uploaded

---
+++ menu.php
@@ -0,0 +1,33 @@
+<!-- Slideshow -->
+<div class="w3-content" style="max-width:500px; margin-bottom:80px">
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/gst.jpeg" alt="Los Angeles">
+    </div>
+    <div class="carousel-item">
+      <img src="image/tax.jpeg" alt="Chicago">
+    </div>
+    <div class="carousel-item">
+      <img src="image/book.jpeg" alt="New York">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+</div>
